6627062e23ab27d9d97b3de66276114769d9d37a
gara-age <minseo00515@naver.com>
2022-02-26 12:10:25 +0900

    닉네임을 받기 위한 화면 이동 명시   - startActivityForResult 활용

--- app/src/main/java/com/example/intent_20220226/MainActivity.kt
+++ app/src/main/java/com/example/intent_20220226/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+//    멤버변수 : 코드 초반부 첫번쩨 { 에서 바로 만드는 변수
+    val REQ_CODE_NICKNAME = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,5 +36,11 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        btnEditNickname.setOnClickListener {
+            val myIntent = Intent(this,EditNicknameActivity::class.java)
+            startActivityForResult(myIntent, REQ_CODE_NICKNAME) // 1000 번은 닉네임을 변경하러 간다는 의미로 사용
+
+
+        }
     }
 }
