0a424b8e1d7331c67272e920cf1e6b6b3d6b2fed
gara-age <minseo00515@naver.com>
2022-02-26 12:17:29 +0900

    닉네임 변경 화면의 저장버튼   - 입력한 닉네임을 putExtra => 메인으로 복귀

--- app/src/main/java/com/example/intent_20220226/EditNicknameActivity.kt
+++ app/src/main/java/com/example/intent_20220226/EditNicknameActivity.kt
@@ -1,11 +1,26 @@
 package com.example.intent_20220226
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+        btnSave.setOnClickListener {
+            val inputNickname = btnedtNickname.text.toString()
+
+            val resultIntent = Intent() //출발지 도착지 명시X
+            resultIntent.putExtra("nick",inputNickname)
+
+            setResult(RESULT_OK,resultIntent)
+
+            finish()
+        }
+
+
     }
 }
